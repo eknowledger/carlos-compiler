7c51faeaf34a6e2863380635bbd9fe37ddb508f5
Ray Toal <rtoal@lmu.edu>
2016-01-06 18:19:24 -0800

    Unit tests for break statements

--- test/test_alive.js
+++ test/test_alive.js
@@ -3,9 +3,26 @@ var fs = require('fs');
 var ohm = require('ohm-js');
 
 describe('The compiler', () => {
+  var contents = fs.readFileSync('./carlos.ohm');
+  var grammar = ohm.grammar(contents);
+
   it('is alive', () => {
-    var contents = fs.readFileSync('./carlos.ohm');
-    var myGrammar = ohm.grammar(contents);
-    console.log(myGrammar);
+    grammar.should.be.ok;
+    grammar.name.should.equal('Carlos');
+  });
+
+  it('can parse break statements', () => {
+    var program = "  break ; break;    break;"
+    grammar.match(program).succeeded().should.be.true;
+  });
+
+  it('rejects the empty program', () => {
+    var program = ""
+    grammar.match(program).succeeded().should.be.false;
+  });
+
+  it('rejects a misspelled break statement', () => {
+    var program = "  break ; break;    breakz;"
+    grammar.match(program).succeeded().should.be.false;
   });
 });
